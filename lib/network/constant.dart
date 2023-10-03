6f93f0270ace4ba0028fc70307186f4d22f8341e
GatutS <gatutsugiantoro60@gmail.com>
2023-10-03 09:59:02 +0700

    update_constant_tabungan

--- lib/network/constant.dart
+++ lib/network/constant.dart
@@ -53,7 +53,8 @@ class Constant {
   static const String tahunAjaran = "/tahun_ajaran.php";
   static const String uploadBukti = "/upload_bukti_bayar.php";
   static const String detailTransaksi = "/detail_transaksi.php";
-  static const String ipaymu_tabungan = "/insert_ipaymu_tabungan.php";
+  // static const String ipaymu_tabungan = "/insert_ipaymu_tabungan.php";
+  static const String ipaymu_tabungan = "/insert_ipaymu_tabunganall.php";
   static const String transaksiTabungan = "/list_transaksi_tabungan.php";
   static const String detailtransaksiTabungan =
       "/detail_transaksi_tabungan.php";
